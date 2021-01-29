bac2d48f0fe5115acae606424c26b78288d97f76
glamboyosa <ogbemudiatimothy@gmail.com>
2021-01-29 16:35:22 +0100

    fix: db connection error

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,5 +1,5 @@
 const db =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV !== 'production'
     ? process.env.DEV_DB
     : process.env.PROD_DB;
 const __PROD__ = process.env.NODE_ENV === 'production';
